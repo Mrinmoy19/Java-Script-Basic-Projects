a57e36cc1294f546c4f0f010081f00394e485232
Mrinmoy19 <mrinmoydey19@gmail.com>
2023-09-01 02:22:13 +0530

    Using HTML events

---
+++ Cost Calculator/logics.js
@@ -0,0 +1,41 @@
+
+// This function is called when the calculate button is pressed.
+
+function calc(){
+    let total = Number(document.querySelector('#js-order-value').value);
+
+    if(total < 500){
+        document.querySelector('shipping_cost').innerHTML = 50;
+        total += 50;  
+    }
+    else{
+        document.querySelector('shipping_cost').innerHTML = 0;
+    }
+
+    document.querySelector('total_cost').innerHTML = total; 
+}
+
+function calc_live(){
+    let total = Number(document.querySelector('#js-order-value_live').value);
+
+    if(total < 500){
+        document.querySelector('shipping_cost_live').innerHTML = 50;
+        total += 50;  
+    }
+    else{
+        document.querySelector('shipping_cost_live').innerHTML = 0;
+    }
+
+    document.querySelector('total_cost_live').innerHTML = total; 
+}
+
+function handle_cost_input(event){
+    if(event.key == 'Enter')
+    {
+        calc();
+    }
+}
+
+function handle_cost_input2(event){
+    calc_live();
+}
